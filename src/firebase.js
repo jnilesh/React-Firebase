48db02f328e765ca6097ba302f3581e3a7f0b294
Nilesh Jain <njain.we@gmail.com>
2020-09-04 13:28:52 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDdle_lonMTLhdZeReuw1QF5LnQlvyF7Q",
+    authDomain: "facebook-njain.firebaseapp.com",
+    databaseURL: "https://facebook-njain.firebaseio.com",
+    projectId: "facebook-njain",
+    storageBucket: "facebook-njain.appspot.com",
+    messagingSenderId: "23084279121",
+    appId: "1:23084279121:web:8d6c97154a3a2c2db51f70",
+    measurementId: "G-X3H629FY52"
+  };
